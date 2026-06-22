351f168999ff34479853931a9d14486ed7136bf8
Joao Vitor <bortoliero.10@hotmail.com>
2026-06-22 19:32:56 -0300

    fix: update CI workflow and Cypress configuration for improved testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "2himvj",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
 });
+
+
+// CYPRESS_RECORD_KEY 8f32e443-6bc9-4daf-b1d8-edba25086748
